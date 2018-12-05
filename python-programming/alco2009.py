3b013e21b6bef002d03af372893c17dc8d890139
EUNHWA LEE <45028757+EUNHWAA@users.noreply.github.com>
2018-12-05 06:41:11 +0000

    Update alco2009.py

--- python-programming/alco2009.py
+++ python-programming/alco2009.py
@@ -8,3 +8,9 @@ print(alco2009.sum())
 alco=pd.read_csv("niaaa-report.csv", index_col=["State","Year"])
 alco["Total"]=alco.Wine+alco.Spirits+alco.Beer
 print(alco.head())
+
+cats=pd.cut(alco2009["Wine"],3).lables
+print(cats)
+wine_state=alco2009["Wine"]>alco2009["Wine"].mean()
+beer_state=alco2009["Beer"]>alco2009["Beer"].mean()
+pd.crosstab(wine_state,beer_state)
