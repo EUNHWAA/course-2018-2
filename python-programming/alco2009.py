d16f369d2da354b8d136a0eeffcf3ea48c00d25f
EUNHWA LEE <45028757+EUNHWAA@users.noreply.github.com>
2018-12-05 03:01:34 +0000

    Update alco2009.py

--- python-programming/alco2009.py
+++ python-programming/alco2009.py
@@ -1,2 +1,8 @@
 import pandas as pd
 alco2009=pd.read_csv("niaa-report2009.csv", index_col="State")
+
+print(alco2009.max())
+print(alco2009.min(axis=1))
+print(alco2009.sum())
+
+alco=pd.read.csv("niaaa-report2009.csv", index_col=["State","Year"])
