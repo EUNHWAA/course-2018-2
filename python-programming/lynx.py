0144386276dd3c29be3197b713cff0a2d72fa52b
Eunhwa Lee <45028757+EUNHWAA@users.noreply.github.com>
2018-12-07 13:24:38 +0900

    Update lynx.py

--- python-programming/lynx.py
+++ python-programming/lynx.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 lynx1=pd.read_csv("lynx.csv")
 lynx1
-lynx1["time"]=round(lynx1["time"]/10)
+lynx1["time"]=round(lynx1["time"]/10)*10
 sum_lynx1=lynx1.groupby("time").sum()
 sum_lynx1.sort_values("time",ascending=False)
